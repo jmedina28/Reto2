3105f024d9da99180e80c56f9507768af727dfb1
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-05 15:55:17 +0200

    creo la ultima capa con la funcion sigmoide

--- Parte 2/swag.py
+++ Parte 2/swag.py
@@ -18,3 +18,4 @@ training_targets = dataset[:, 8]
 model = Sequential()
 model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
