c3dbf6de2074cba80139dcd0bf6539ec1b1aa94a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-05 16:06:36 +0200

    añadidos comentarios y cambios en los nombres de las variables

--- Parte 2/swag.py
+++ Parte 2/swag.py
@@ -3,11 +3,11 @@ from keras.layers import Dense
 import numpy
 import matplotlib.pyplot as plt
 
-num = int(input("¿Cuantas veces quieres entrenar la red neuronal?\n"))
-tamaño = int(input("¿Cuantas veces quieres ejecutar el entreno de la red neuronal?\n"))
-sem = int(input("¿Cual es la semilla de la red neuronal?\n"))
+num = int(input("¿Cuantas veces quieres entrenar la red neuronal?\n")) # epoch
+tamaño = int(input("¿Cuantas veces quieres ejecutar el entreno de la red neuronal?\n")) #bach size
+raiz = int(input("¿Cual es la raíz de la red neuronal?\n")) #seed
 
-numpy.random.seed(sem)
+numpy.random.seed(raiz)
 
 dataset = numpy.loadtxt("Parte 2\swag.csv", delimiter=',')
 
