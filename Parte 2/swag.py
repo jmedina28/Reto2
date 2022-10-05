b9490b16b717e5593dda6ac05c529ac5e3cab50d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-05 15:55:38 +0200

    compilo el modelo con la precisión

--- Parte 2/swag.py
+++ Parte 2/swag.py
@@ -19,3 +19,5 @@ model = Sequential()
 model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
