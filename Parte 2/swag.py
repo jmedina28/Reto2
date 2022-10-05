a05619ec72ac105744e7b0778ef5565314f6e2bb
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-05 15:52:15 +0200

    creo los imports respectivos a las variables de las redes neuronales

--- Parte 2/swag.py
+++ Parte 2/swag.py
@@ -2,3 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 import matplotlib.pyplot as plt
+
+num = int(input("¿Cuantas veces quieres entrenar la red neuronal?\n"))
+tamaño = int(input("¿Cuantas veces quieres ejecutar el entreno de la red neuronal?\n"))
+sem = int(input("¿Cual es la semilla de la red neuronal?\n"))
