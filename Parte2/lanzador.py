1969c1d3fd3329a14914f921468da2ca51e76833
jmedina28 <juanperezmedina67@gmail.com>
2022-10-09 21:33:17 +0200

    tildes

--- Parte2/lanzador.py
+++ Parte2/lanzador.py
@@ -4,7 +4,7 @@ def lanzar():
     while eleccion != "2":
         eleccion = input("¿Qué quieres hacer?\n(1) Ejecutar el Algoritmo Swag\n(2) Salir\nElección: ")
         if eleccion == "1":
-            num = int(input("¿Cuantas veces quieres entrenar la red neuronal?\n")) # epoch
-            tamaño = int(input("¿Cuantas veces quieres ejecutar el entreno de la red neuronal?\n")) #bach size
+            num = int(input("¿Cuántas veces quieres entrenar la red neuronal?\n")) # epoch
+            tamaño = int(input("¿Cuántas veces quieres ejecutar el entreno de la red neuronal?\n")) #bach size
             raiz = int(input("¿Cual es la raíz de la red neuronal?\n")) #seed
             swag(num, tamaño, raiz)
